55dc471ae057a607989cc9a376eecca951367469
曾英峰 <1666127255@qq.com>
2019-01-17 19:13:33 +0800

    基本框架

--- main.c
+++ main.c
@@ -2,7 +2,36 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("huanyuingjinruzhuanchangweizhiguanlixitong\n");
+
+    while(1){
+        printf("1----tianjia\n");
+        printf("2----shanchu\n");
+        printf("3----xianshiquanbu\n");
+        printf("4----fanhuiwodeweizhi\n");
+        printf("5----tuichu\n");
+
+        printf("xuanzegongnneng\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            printf("tuichu\n");
+            break;
+        }
+    }
 
     return 0;
 
